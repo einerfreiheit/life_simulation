822c30cdf50078cf8601ce76c5dc889937cf3d6c
einerfreiheit <einerfreiheit@gmail.com>
2017-02-22 01:39:14 +0300

    бага пофикшена

--- wormlogic.cpp
+++ wormlogic.cpp
@@ -8,13 +8,14 @@
 #include <cstdlib>
 #include "wormlogic.h"
 
-wormlogic::wormlogic()
+wormlogic::wormlogic(int y,int x)
 {
     hungry = false;
     way = -1;
     next_x = 0;
     next_y = 0;
-
+    this->size_y=y;
+    this->size_x=x;
 }
 double wormlogic::eat(double food)
 {
@@ -34,7 +35,7 @@ bool wormlogic::IsHungry(double food)
         hungry = false;
     return hungry;
 }
-void wormlogic::move(bool hungry, int y, int x, int size_y, int size_x)
+void wormlogic::move(bool hungry, int y, int x)
 {
     next_x = x;
     next_y = y;
@@ -49,7 +50,7 @@ void wormlogic::move(bool hungry, int y, int x, int size_y, int size_x)
 				break;
 		        }
         	case up: {
-        		if (y < size_y)
+        		if (y < size_y-1)
         		         next_y = y + 1;
         	    else
         		         next_y = y - 1;
@@ -63,7 +64,7 @@ void wormlogic::move(bool hungry, int y, int x, int size_y, int size_x)
         		 break;
         	}
         	case right:{
-        		  if (x < size_x)
+        		  if (x < size_x-1)
         		         next_x = x + 1;
         		  else
         		         next_x = x - 1;
