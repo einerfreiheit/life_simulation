a140c14dbdd76977dc62eeff326cd5594bce650e
einerfreiheit <einerfreiheit@gmail.com>
2017-02-28 17:50:25 +0300

    работа над спавном червей

--- Workers/VisualWorker.h
+++ Workers/VisualWorker.h
@@ -8,14 +8,12 @@ public:
 	VisualWorker();
 	virtual ~VisualWorker();
 	void work (World *world) override;
-	void computingImage(World *world);
-	void showImage(World *world);
+
 	std::vector<Creature> worm_map ;
 	std::vector<std::vector<Cell> > map;
-	 int mapHeight;
-	 int mapWidth;
 private:
-    void getImageNICE(World *world);
+	void computingImage(World *world);
+	void showImage(World *world);
 };
 
 #endif /* WORKERS_VISUALWORKER_H_ */
