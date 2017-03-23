a6f79e277699f828324274f7921a6484a6355dfb
einerfreiheit <einerfreiheit@gmail.com>
2017-03-23 21:18:43 +0300

    добавил абстрактный класс визуализации+ визуализацию малого мира

---
+++ Visualization/Visualization.h
@@ -0,0 +1,15 @@
+#ifndef VISUALIZATION_VISUALIZATION_H_
+#define VISUALIZATION_VISUALIZATION_H_
+#include"../CommonIncludes.h"
+#include "../World.h"
+
+class Visualization {
+public:
+	Visualization();
+	virtual ~Visualization();
+	virtual void visualize(World *world)=0;
+	virtual void computeImage(World *world)=0;
+	virtual void showImage(World *world)=0;
+};
+
+#endif
