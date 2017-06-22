d5088b00371857b5aa95557e7990b50db8a58d3f
einerfreiheit <einerfreiheit@gmail.com>
2017-06-22 14:50:36 +0300

    удален билдер высот  diamond square

--- Factories/HeightsBuilder.cpp
+++ Factories/HeightsBuilder.cpp
@@ -1,16 +1,15 @@
 #include "HeightsBuilder.h"
 #include "../SimulationData.h"
-#include "../Map/DiamondSquare.h"
 #include "../Map/ValueNoise.h"
 #include "../Map/PlyConverter.h"
 #include "../Map/PlyModel.h"
 #include "../Map/PlyExporter.h"
 
 void HeightsBuilder::build(World* world) {
-	//@ выбор типа генератора мира из конфига
-	ValueNoise mapGen;
-	mapGen.makeNoise(world);
-	//DiamondSquare diamondSquare ( world );//@ переписать diamond square
+
+	ValueNoise valueNoise;
+	valueNoise.makeNoise(world);
+
 
 	PlyModel *model = new PlyModel;
 	PlyConverter::setVertices(world, model);
